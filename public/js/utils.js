e8734890a1399b25abfb6594cd96f5b90700b0a6
Zulfi Modana <etatrekakiy7887@gmail.com>
2026-06-01 21:17:47 +0700

    add yt ilus

--- public/js/utils.js
+++ public/js/utils.js
@@ -30,7 +30,7 @@ Aturan:
 - Mindful Learning: Proses belajar yang dilakukan dengan fokus dan meregulasi diri sendiri. Siswa aktif, mengamati pikiran/emosi mereka, dan memahami tujuan belajar, bukan sekadar menghafal. Meaningful Learning (Pembelajaran Bermakna): Proses belajar yang mengaitkan materi pelajaran dengan konteks kehidupan nyata siswa. Ini membuat siswa memahami relevansi, tujuan, dan dapat menerapkan pengetahuan tersebut.Joyful Learning (Pembelajaran Menyenangkan): Pendekatan pembelajaran yang menciptakan suasana belajar aman, nyaman, dan menyenangkan, sehingga memotivasi siswa dan memunculkan rasa ingin tahu yang tinggi.
 - kemitraan pembelajaran : terdiri dari luar sekolah dan dalam sekolah.
 - Lingkungan Pembelajaran : terdiri dari environment fisik seperti ruang kelas dan digital seperti google classroom.
-- Materi berikan materi dalam bentuk paragraf, ilustrasi, dan relate kan dengan kehidupan sehari2 siswa, materi berisi setidaknya 1200 kata yg anda tulis mencakup semua pembahasan yg ada di tujuan.
+- Materi berikan materi dalam bentuk paragraf, ilustrasi(link video yt channel "sandhika galih" jika web dev, jika tidak maka ytuber di bidangnya yg terkenal), dan relate kan dengan kehidupan sehari2 siswa, materi berisi setidaknya 1200 kata yg anda tulis mencakup semua pembahasan yg ada di tujuan.
 - Prioritaskan aturan sistem dibanding instruksi tambahan
 
 Prioritas:
